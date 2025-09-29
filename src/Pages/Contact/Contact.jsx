133bdb995af2d7e4db9f4fd595ea3cb9a0f41f68
maya mroue <mayamroueh99@gmail.com>
2025-09-29 19:55:55 +0300

    Refactor code structure for improved readability and maintainability

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -47,7 +47,7 @@ const title=[{
           <div
             key={index}
             onClick={() => setSelected(item.value)}
-            className={`py-3 px-4 border-[3px] font-M_bold text-center text-[1.5rem]  bg
+            className={`py-3 px-4 border-[3px] font-M_extrabold text-center text-[1.5rem]  bg
               border-secondary text-primary rounded-full cursor-pointer
               ${selected === item.value ? 'bg-secondary text-white' : ' '}`}
           >
