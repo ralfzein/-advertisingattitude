7b7b5179fe73df5127c98a0a076ffcd5462ef95e
maya mroue <mayamroueh99@gmail.com>
2025-10-08 19:14:01 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
    optimizeDeps: {
-    include: ["scroll-snap"]
-  }
+    include: ["scroll-snap",'@studio-freight/lenis']
+  },
+  
 })
