2568408674fff954aea22c2879906a436740ec23
maya mroue <mayamroueh99@gmail.com>
2025-11-01 08:25:48 +0200

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './index.css'
 import AppRoutes from './Routes'
-import Footer from './components/Footer/Footer'
 import { Toaster } from 'sonner'
 
 function App() {
